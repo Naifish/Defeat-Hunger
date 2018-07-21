75c502a5941748f34ba5a06ec755de104fb5505b
Naifish Ali <nf533051@dal.ca>
2018-07-20 23:02:47 -0300

    Donors page ui done

--- donors.php
+++ donors.php
@@ -6,154 +6,48 @@ require 'includes/header.php';
 ?>
 <!-- Reference from https://www.w3schools.com/howto/howto_css_contact_section.asp -->
 
-<div class="container our-donors">
-
-    <div class="text-center eventForm">
-        <div class="text-center"><h3><b>Our Donors</b></h3></div>
-        <div class="container" style="border-style:groove;">
-            <div class="media-left media-middle">
-                <!--Image courtesy https://goo.gl/images/doPgVQ-->
-                <img src="images/download3.png" class="media-object" style="width:180px; height:150px">
-            </div>
-            <div class="media-body">
-                <form class="form-horizontal" name="validation" action="welcome.php">
-                    <div class="form-group">
-                        <label class="control-label col-sm-2" for="name">Name/Organisation Name:</label>
-                        <div class="col-sm-10">
-                            <input type="text" class="form-control" id="event" value="Donor/Organisation Name"
-                                   name="event" required>
-                        </div>
-                        <label class="control-label col-sm-2" for="Venue">Address:</label>
-                        <div class="col-sm-10">
-                            <input type="text" class="form-control" id="venue" value="55666, south street, Darthmouth "
-                                   name="venue" required>
-                        </div>
-                        <label class="control-label col-sm-2" for="Venue">Email/Website:</label>
-                        <div class="col-sm-10">
-                            <input type="email" class="form-control" id="email" value="www.worldHealthorganisation.com"
-                                   name="venue" required>
-                        </div>
-                        <label class="control-label col-sm-2" for="name">Type of Membership:</label>
-                        <div class="col-sm-10">
-                            <input type="text" class="form-control" id="event" value="Premium" name="event" required>
-                        </div>
-
-                    </div>
-                </form>
-
-            </div>
-        </div>
-
-        <!---Template refrences https://www.w3schools.com/bootstrap/bootstrap_media_objects.asp-->
-
-        <div class="container" style="border-style:groove;">
-            <div class="media-left media-middle">
-
-                <!-- image courtesy https://goo.gl/images/BXJyJp -->
-                <img src="images/download2.png" class="media-object" style="width:180px; height:150px">
-            </div>
-            <div class="media-body">
-
-                <form class="form-horizontal" name="validation" action="welcome.php">
-                    <div class="form-group">
-                        <label class="control-label col-sm-2" for="name">Name/Organisation Name:</label>
-                        <div class="col-sm-10">
-                            <input type="text" class="form-control" id="event" value="Donor/Organisation Name"
-                                   name="event" required>
-                        </div>
-                        <label class="control-label col-sm-2" for="Venue">Address:</label>
-                        <div class="col-sm-10">
-                            <input type="text" class="form-control" id="venue" value="55666, south street, Darthmouth "
-                                   name="venue" required>
-                        </div>
-                        <label class="control-label col-sm-2" for="Venue">Email/Website:</label>
-                        <div class="col-sm-10">
-                            <input type="email" class="form-control" id="email" value="www.worldHealthorganisation.com"
-                                   name="venue" required>
-                        </div>
-                        <label class="control-label col-sm-2" for="name">Type of Membership:</label>
-                        <div class="col-sm-10">
-                            <input type="text" class="form-control" id="event" value="Regular" name="event" required>
-                        </div>
-
-                    </div>
-                </form>
-
-            </div>
-
-
-        </div>
-        <div class="container" style="border-style:groove;">
-            <div class="media-left media-middle">
-                <img src="images/download1.png" class="media-object" style="width:180px; height:150px">
-                <!--image refrence - https://goo.gl/images/t1ujQC-->
-            </div>
-            <div class="media-body">
-
-                <form class="form-horizontal" name="validation" action="welcome.php">
-                    <div class="form-group">
-                        <label class="control-label col-sm-2" for="name">Name/Organisation Name:</label>
-                        <div class="col-sm-10">
-                            <input type="text" class="form-control" id="event" value="Donor/Organisation Name"
-                                   name="event" required>
-                        </div>
-                        <label class="control-label col-sm-2" for="Venue">Address:</label>
-                        <div class="col-sm-10">
-                            <input type="text" class="form-control" id="venue" value="55666, south street, Darthmouth "
-                                   name="venue" required>
-                        </div>
-                        <label class="control-label col-sm-2" for="Venue">Email/Website:</label>
-                        <div class="col-sm-10">
-                            <input type="email" class="form-control" id="email" value="www.worldHealthorganisation.com"
-                                   name="venue" required>
-                        </div>
-                        <label class="control-label col-sm-2" for="name">Type of Membership:</label>
-                        <div class="col-sm-10">
-                            <input type="text" class="form-control" id="event" value="Premium" name="event" required>
-                        </div>
-
-                    </div>
-                </form>
-
-            </div>
-
-        </div>
-        <div class="container" style="border-style:groove;">
-            <div class="media-left media-middle">
-                <!--Image courtesy https://goo.gl/images/doPgVQ-->
-                <img src="images/download3.png" class="media-object" style="width:180px; height:150px">
-            </div>
-            <div class="media-body">
-
-                <form class="form-horizontal" name="validation" action="welcome.php">
-                    <div class="form-group">
-                        <label class="control-label col-sm-2" for="name">Name/Organisation Name:</label>
-                        <div class="col-sm-10">
-                            <input type="text" class="form-control" id="event" value="Donor/Organisation Name"
-                                   name="event" required>
-                        </div>
-                        <label class="control-label col-sm-2" for="Venue">Address:</label>
-                        <div class="col-sm-10">
-                            <input type="text" class="form-control" id="venue" value="55666, south street, Darthmouth "
-                                   name="venue" required>
-                        </div>
-                        <label class="control-label col-sm-2" for="Venue">Email/Website:</label>
-                        <div class="col-sm-10">
-                            <input type="email" class="form-control" id="email" value="www.worldHealthorganisation.com"
-                                   name="venue" required>
-                        </div>
-                        <label class="control-label col-sm-2" for="name">Type of Membership:</label>
-                        <div class="col-sm-10">
-                            <input type="text" class="form-control" id="event" value="Premium" name="event" required>
-                        </div>
-
-                    </div>
-                </form>
-
-            </div>
+<div class="container container-donors">
+    <h1>Our Donors</h1>
+    <div class="row">
+        <!-- Image URL: https://pngtree.com/freepng/medals_541578.html -->
+        <div class="col-md-2 donor-gold"></div>
+        <div class="col-md-10">
+            <h2>Name: Mezza Fast food</h2>
+            <h3>Contact: (333) 333-4543</h3>
+            <h4>Email: <a href="mailto:test@live.com">test@live.com</a></h4>
+            <h5>Type: Gold</h5>
         </div>
     </div>
-</div>
+    <div class="row">
+        <!-- Image URL: https://pngtree.com/freepng/medals_541578.html -->
+        <div class="col-md-2 donor-silver"></div>
+        <div class="col-md-10">
+            <h2>Name: Mezza Fast food</h2>
+            <h3>Contact: (333) 333-4543</h3>
+            <h4>Email: <a href="mailto:test@live.com">test@live.com</a></h4>
+            <h5>Type: Gold</h5>
+        </div>
+    </div>
+    <div class="row">
+        <!-- Image URL: https://pngtree.com/freepng/medals_541578.html -->
+        <div class="col-md-2 donor-bronze"></div>
+        <div class="col-md-10">
+            <h2>Name: Mezza Fast food</h2>
+            <h3>Contact: (333) 333-4543</h3>
+            <h4>Email: <a href="mailto:test@live.com">test@live.com</a></h4>
+            <h5>Type: Gold</h5>
+        </div>
+    </div>
+    <div class="row">
+        <!-- Image URL: https://pngtree.com/freepng/medals_541578.html -->
+        <div class="col-md-2 donor-silver"></div>
+        <div class="col-md-10">
+            <h2>Name: Mezza Fast food</h2>
+            <h3>Contact: (333) 333-4543</h3>
+            <h4>Email: <a href="mailto:test@live.com">test@live.com</a></h4>
+            <h5>Type: Gold</h5>
+        </div>
+    </div>
 </div>
 <?php include 'includes/footer.php'; ?>
 </body>
